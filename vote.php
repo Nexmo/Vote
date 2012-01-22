fc0bbe9f191de124bb5d6203be5b436a833a83ae
Tim Lytle <tim@timlytle.net>
2012-01-21 19:52:48 -0500

    Removed debug/testing code (enabled limits).

--- vote.php
+++ vote.php
@@ -9,57 +9,68 @@ require_once 'lib/nexmo/NexmoMessage.php';
 
 //validate it's from nexmo
 $nexmo = new NexmoMessage($config['nexmo']['user'], $config['nexmo']['pass']);
-if(!$nexmo->inboundText($_REQUEST)){
-//    header('HTTP/1.0 404 Not Found');
-//    exit;
+//this just checks for the expects fields
+if(!$nexmo->inboundText($_POST)){
+    header('HTTP/1.0 404 Not Found');
+    exit;
 }
 
-//TODO: match to whitelist
+//TODO: would be nice to have an optional whitelist of choices 
 
 //parse vote
 //pull out all hash tags
 if(preg_match_all('/#(\w*)/', $nexmo->text, $votes)){
     foreach($votes[1] as $vote){
-        //TODO: accept multiple votes?
+        //TODO: could allow multiple votes here (but still limit to one SMS)
         break; //first vote is all that counts
     }
 } else {
-    exit; //no vote
+    exit; //no vote detected, nothing to do
 }
 
 //setup cloudmine
 require_once 'lib/cloudmine/CloudMine.php';
-$cloudmine = new CloudMine($config['cloudmine']['user'], $config['cloudmine']['pass']);
+$cloudmine = new CloudMine($config['cloudmine']['user'], 
+                           $config['cloudmine']['pass']);
 
 /**
  * Add to cloudmine
  */
-//track numbers, to limit voting
-$data = $cloudmine->get('a2sw'); //yeah, pulling back all the data here
+//TODO: yeah, pulling back all the data here, this could be better modeled
+//look at us, using a key that matches the event, it's like we're thinking of
+//adding an admin UI layer allowing multiple events
+$data = $cloudmine->get('a2sw'); 
+
+//TODO: make this check optional
 if(isset($data['a2sw']['voters'][$nexmo->from])){
-    //return; //only count the first 
+    return; //only count the first vote
 }
 
-//use message id so it's unique
 $data = array();
-$data['a2sw']['votes'][$vote][$_REQUEST['messageId']] = array('to' => $nexmo->to, 'text' => $nexmo->text, 'from' => $nexmo->from, 'time' => time());
+//add vote to the matched choice (using the message id as the key)
+$data['a2sw']['votes'][$vote][$_REQUEST['messageId']] 
+    = array('to' => $nexmo->to, 
+    		'text' => $nexmo->text, 
+    		'from' => $nexmo->from, 
+    		'time' => time());
+//track the voters, so we can limit to one vote per phone
 $data['a2sw']['voters'][$nexmo->from] = $data['a2sw']['votes'][$vote];
 
+//update the data (merging with the exsisting data)
 $cloudmine->update($data);
 
-//publish to pubnub
+//publish to pubnub, updating the chart in real time
 require_once 'lib/Pubnub.php';
 
 $pubnub = new Pubnub(
-    $config['pubnub']['pub'],  ## PUBLISH_KEY
-    $config['pubnub']['sub'],  ## SUBSCRIBE_KEY
-    "",      ## SECRET_KEY
-    false    ## SSL_ON?
+    $config['pubnub']['pub'],
+    $config['pubnub']['sub'],
+    "", //secret key, no need for that
+    false //not using ssl
 );
 
-$info = $pubnub->publish(array(
-    'channel' => 'a2sw', ## REQUIRED Channel to Send
-    'message' => $vote   ## REQUIRED Message String/Array
+$pubnub->publish(array(
+    'channel' => 'a2sw', //look at that, this should be a variable in the config
+    'message' => $vote //the data is just the text of the vote
 ));
 
-error_log(var_export($info, true));
