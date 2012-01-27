4c8c8c28c629878be9759f91c6193a2e8d71ea0f
Tim Lytle <tim@timlytle.net>
2012-01-27 10:36:43 -0500

    Added sample config file.

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+return array(
+    'cloudmine' => array('user' => '', 'pass' => ''),
+    'nexmo' => array('user' => '', 'pass' => '', 'number' => '1-123-555-1234'),
+    'pubnub' => array('pub' => '', 'sub' => '')
+);
+
